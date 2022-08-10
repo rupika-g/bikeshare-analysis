d045f7f14e818584fc32cb820f9e72aa12b4ff57
Rupika <email>
2022-08-09 20:22:07 -0700

    Cleanup

--- cleaning.R
+++ cleaning.R
@@ -5,8 +5,9 @@ library(janitor)
 library(rlang)
 # library(hms)
 
-
-process_data <- function(data_frame, type_of_rider) {
+# Calculate ride duration and mean ride duration
+# Return modified data frame with ride duration column
+calculate_trip_duration <- function(data_frame, type_of_rider) {
   data_frame$trip_duration <- difftime(data_frame$ended_at, data_frame$started_at)
   data_frame <- arrange(data_frame, -trip_duration)
   data_frame <- clean_names(data_frame)
@@ -15,46 +16,47 @@ process_data <- function(data_frame, type_of_rider) {
   return(data_frame)
 }
 
-process_csv <- function(file_name) {
-  df <- read_csv(file_name)
-  View(df, "Full")
-
-  glimpse(df)
-  
-  member_df <- df %>% filter(member_casual == "member")
-  casual_df <- df %>% filter(member_casual == "casual")
-  
-  process_events(member_df, "Member")
-  process_events(casual_df, "Casual")
-  
-  # casual_df %>% drop_na %>%
-  # summarize(total_rideable = count(casual_df,rideable_type))
-}
-
-process_events <- function(sub_df, df_name) {
+total_riders_per_station <- function(sub_df, df_name) {
   sub_df %>% group_by(rideable_type) %>% drop_na %>%
     summarize(total_rideable_type = length(rideable_type))
-  #unique(sub_df$rideable_type) 
-  
-  
-  #sub_df <- process_data(sub_df, "casual")
-  #View(casual_df)
-  
-  #sub_df <- process_data(sub_df, "member")
-  #View(sub_df)
-  
-  # sub_df %>% group_by(rideable_type)%>% drop_na %>%
-  #   summarize(total_rideable_type = length(rideable_type))
-  
-  # sub_df %>% drop_na %>%
-  #   summarize(total_rideable_type = count(sub_df,rideable_type))
   
   new_sub_df <- sub_df %>% drop_na() %>% count(start_station_name, end_station_name, sort= TRUE)
   
   View(new_sub_df, df_name)
 }
+
+rideable_types <- function(sub_df, df_name) {
+  types <- sub_df %>% group_by(rideable_type)%>% drop_na %>%
+    summarize(total_rideable_type = length(rideable_type))
+  print(paste(df_name, types))
+}
+
+process_csv <- function(file_name) {
+  # Load data
+  df <- read_csv(file_name)
   
+  # Clean data
+  df <- remove_empty(df, which=c("rows"))
+  df <- remove_empty(df, which=c("cols"))
+  #View(df, "Full")
 
+  glimpse(df)
 
+  # Create data frames for member and casual riders
+  casual_df <- df %>% filter(member_casual == "casual")
+  member_df <- df %>% filter(member_casual == "member")
+  
+  # Analyze data
+  
+  casual_df <- calculate_trip_duration(casual_df, "casual")
+  member_df <- calculate_trip_duration(member_df, "member")
+  
+  total_riders_per_station(casual_df, "Casual")
+  total_riders_per_station(member_df, "Member")
+
+  unique(casual_df$rideable_type)
+  rideable_types(casual_df, "Casual")
+  rideable_types(member_df, "Member")
+}
 
 process_csv("C:/Users/rramachandran/Downloads/Data Analysis Capstone Project/202106-divvy-tripdata.csv")
