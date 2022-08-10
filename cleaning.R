58d1e19eb4ec420541821afcd82f8df6bb8cdaa0
Rupika <email>
2022-08-10 00:30:41 -0700

    Process data for the whole year Display mean trip duration using ggplot

--- cleaning.R
+++ cleaning.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(readr)
 library(janitor)
 library(rlang)
+library(lubridate)
+library(ggplot2)
 # library(hms)
 
 # Calculate ride duration and mean ride duration
@@ -13,16 +15,18 @@ calculate_trip_duration <- function(data_frame, type_of_rider) {
   data_frame <- clean_names(data_frame)
   mean_trip_duration <- mean(data_frame$trip_duration)
   print(paste("Mean", type_of_rider, mean_trip_duration))
-  return(data_frame)
+  year_and_month <- paste(month(data_frame$started_at[1]), "/", year(data_frame$started_at[1]))
+  return(list("mean" = mean_trip_duration, "month" = year_and_month))
 }
 
 total_riders_per_station <- function(sub_df, df_name) {
   sub_df %>% group_by(rideable_type) %>% drop_na %>%
     summarize(total_rideable_type = length(rideable_type))
   
-  new_sub_df <- sub_df %>% drop_na() %>% count(start_station_name, end_station_name, sort= TRUE)
+  # Most popular 10 routes
+  new_sub_df <- head(sub_df %>% drop_na() %>% count(start_station_name, end_station_name, sort= TRUE), 10)
   
-  View(new_sub_df, df_name)
+  #View(new_sub_df, df_name)
 }
 
 rideable_types <- function(sub_df, df_name) {
@@ -40,7 +44,7 @@ process_csv <- function(file_name) {
   df <- remove_empty(df, which=c("cols"))
   #View(df, "Full")
 
-  glimpse(df)
+  #glimpse(df)
 
   # Create data frames for member and casual riders
   casual_df <- df %>% filter(member_casual == "casual")
@@ -48,8 +52,8 @@ process_csv <- function(file_name) {
   
   # Analyze data
   
-  casual_df <- calculate_trip_duration(casual_df, "casual")
-  member_df <- calculate_trip_duration(member_df, "member")
+  casual_trip_details <- calculate_trip_duration(casual_df, "casual")
+  member_trip_details <- calculate_trip_duration(member_df, "member")
   
   total_riders_per_station(casual_df, "Casual")
   total_riders_per_station(member_df, "Member")
@@ -57,6 +61,28 @@ process_csv <- function(file_name) {
   unique(casual_df$rideable_type)
   rideable_types(casual_df, "Casual")
   rideable_types(member_df, "Member")
+
+  return(casual_trip_details)
 }
 
-process_csv("C:/Users/rramachandran/Downloads/Data Analysis Capstone Project/202106-divvy-tripdata.csv")
+csv_file_list = list.files("C:/Users/rramachandran/Downloads/Data Analysis Capstone Project/", pattern="*-divvy-tripdata.csv", full.names=TRUE)
+
+# Create list 
+casual_trip_duration_list <- c()
+casual_trip_month_list <- c()
+
+for (csv_file in csv_file_list) {
+ casual_mean_trip_details <- process_csv(csv_file)
+ casual_trip_duration_list <- append(casual_trip_duration_list, casual_mean_trip_details$mean)
+ casual_trip_month_list <- append(casual_trip_month_list, casual_mean_trip_details$month)
+}
+
+print(casual_trip_duration_list)
+
+# Creating df for Mean
+mean_df <- data.frame(Month=casual_trip_month_list, Mean_casual=casual_trip_duration_list)
+
+ggplot(mean_df, aes(x= Mean_casual, y= Month, group=1)) + geom_line()+ geom_point()
+
+#casual_mean_trip_details <- process_csv("C:/Users/rramachandran/Downloads/Data Analysis Capstone Project/202106-divvy-tripdata.csv")
+#print(casual_mean_trip_details)
