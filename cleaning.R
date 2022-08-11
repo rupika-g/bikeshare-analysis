a1a6ee07a72d70cb5318b685b0372078d035c796
Rupika <email>
2022-08-10 23:49:40 -0700

    Crested new data frame and visualization for total number of riders

--- cleaning.R
+++ cleaning.R
@@ -11,12 +11,13 @@ library(ggplot2)
 # Return modified data frame with ride duration column
 calculate_trip_duration <- function(data_frame, type_of_rider) {
   data_frame$trip_duration <- difftime(data_frame$ended_at, data_frame$started_at)
-  data_frame <- arrange(data_frame, -trip_duration)
+  data_frame <- arrange(data_frame, -trip_duration, started_at)
   data_frame <- clean_names(data_frame)
   mean_trip_duration <- mean(data_frame$trip_duration)
+  total_no_of_riders <- length(data_frame$ride_id)
+  print(total_no_of_riders)
   print(paste("Mean", type_of_rider, mean_trip_duration))
-  year_and_month <- paste(month(data_frame$started_at[1]), "/", year(data_frame$started_at[1]))
-  return(list("mean" = mean_trip_duration, "month" = year_and_month))
+  return(list("month" = data_frame$started_at[1], "mean" = mean_trip_duration, "num_riders" = total_no_of_riders))
 }
 
 total_riders_per_station <- function(sub_df, df_name) {
@@ -62,7 +63,7 @@ process_csv <- function(file_name) {
   rideable_types(casual_df, "Casual")
   rideable_types(member_df, "Member")
 
-  return(casual_trip_details)
+  return(list("casual" = casual_trip_details, "member" = member_trip_details))
 }
 
 csv_file_list = list.files("C:/Users/rramachandran/Downloads/Data Analysis Capstone Project/", pattern="*-divvy-tripdata.csv", full.names=TRUE)
@@ -70,19 +71,37 @@ csv_file_list = list.files("C:/Users/rramachandran/Downloads/Data Analysis Capst
 # Create list 
 casual_trip_duration_list <- c()
 casual_trip_month_list <- c()
+member_trip_duration_list <- c()
+member_trip_month_list <- c()
+
+num_of_casual_riders <- c()
+num_of_member_riders <- c()
 
 for (csv_file in csv_file_list) {
- casual_mean_trip_details <- process_csv(csv_file)
- casual_trip_duration_list <- append(casual_trip_duration_list, casual_mean_trip_details$mean)
- casual_trip_month_list <- append(casual_trip_month_list, casual_mean_trip_details$month)
+  trip_details <- process_csv(csv_file)
+  casual_trip_duration_list <- append(casual_trip_duration_list, trip_details$casual$mean)
+  casual_trip_month_list <- append(casual_trip_month_list, trip_details$casual$month)
+  num_of_casual_riders <- append(num_of_casual_riders, trip_details$casual$num_riders)
+  
+  member_trip_duration_list <- append(member_trip_duration_list, trip_details$member$mean)
+  member_trip_month_list <- append(member_trip_month_list, trip_details$member$month)
+  num_of_member_riders <- append(num_of_member_riders, trip_details$member$num_riders)
 }
 
 print(casual_trip_duration_list)
 
 # Creating df for Mean
-mean_df <- data.frame(Month=casual_trip_month_list, Mean_casual=casual_trip_duration_list)
+mean_df <- data.frame(Month=casual_trip_month_list, Mean_casual=casual_trip_duration_list, Mean_member=member_trip_duration_list)
+#casual_mean_df <- mean_df %>% arrange(mdy(casual_mean_df$Month))
+number_of_riders_df <- data.frame(Month=casual_trip_month_list, casual_riders=num_of_casual_riders, member_riders=num_of_member_riders)
 
-ggplot(mean_df, aes(x= Mean_casual, y= Month, group=1)) + geom_line()+ geom_point()
+# Visualization
+ggplot(mean_df, aes(x= Month)) + 
+  geom_line(aes(y= Mean_casual, color= "green"))+
+  geom_line(aes(y= Mean_member, color= "red"))+
+  theme(axis.text.x = element_text(angle = 45))
 
-#casual_mean_trip_details <- process_csv("C:/Users/rramachandran/Downloads/Data Analysis Capstone Project/202106-divvy-tripdata.csv")
-#print(casual_mean_trip_details)
+ggplot(number_of_riders_df, aes(x= Month)) + 
+  geom_line(aes(y= casual_riders, color= "green"))+
+  geom_line(aes(y= member_riders, color= "red"))+
+  theme(axis.text.x = element_text(angle = 45))
